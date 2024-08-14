31aec57f82c0d02986e1d648daeee60a1236a530
ThiagoS5 <thimarqueti@gmail.com>
2024-08-14 13:01:57 -0300

    chore: adição de lint, prettier e configuração para o stitches

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,53 +1,20 @@
-import Head from "next/head";
-import StitchesLogo from "../components/StitchesLogo";
-import { styled } from "../stitches.config";
+import React from 'react';
+import Head from 'next/head';
+import { styled } from '../stitches.config';
 
-const Box = styled("div", {});
+const Box = styled('div', {});
 
-const Text = styled("p", {
-  fontFamily: "$system",
-  color: "$hiContrast",
-});
-
-const Link = styled("a", {
-  fontFamily: "$system",
-  textDecoration: "none",
-  color: "$purple600",
-});
-
-const Container = styled("div", {
-  marginX: "auto",
-  paddingX: "$3",
-
-  variants: {
-    size: {
-      1: {
-        maxWidth: "300px",
-      },
-      2: {
-        maxWidth: "585px",
-      },
-      3: {
-        maxWidth: "865px",
-      },
-    },
-  },
+const Button = styled('button', {
+  backgroundColor: '$rockeatseat',
 });
 
 export default function Home() {
   return (
-    <Box css={{ paddingY: "$6" }}>
+    <Box css={{ paddingY: '$6' }}>
       <Head>
-        <title>Use Stitches with Next.js</title>
+        <title>My personal project</title>
       </Head>
-      <Container size={{ "@initial": "1", "@bp1": "2" }}>
-        <StitchesLogo />
-        <Text as="h1">Hello, from Stitches.</Text>
-        <Text>
-          For full documentation, visit{" "}
-          <Link href="https://stitches.dev">stitches.dev</Link>.
-        </Text>
-      </Container>
+      <Button> Click here </Button>
     </Box>
   );
 }
